36cd861cfe1ea05b8ea88a7057497711d29443ce
GeorgiosPapavasileiou1 <georpapa167@mbg.duth.gr>
2025-12-29 23:30:01 +0200

    Add files via upload

---
+++ Sunday project/pa3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	char seq[ 1000 ];
+	char penta[ 100 ];
+	int len1, len2;
+	int i, k;
+	int found_it;
+	printf("Enter the target sequence:");
+	scanf("%s", seq);
+
+	len1 = strlen(seq);
+	printf("Enter the sequence : ");
+	scanf("%s", penta);
+	len2 = strlen( penta );
+	printf("The length of the target sequence  was : %d \n", len1);
+	if(len1 < len2 )
+	{
+		printf("Eisai boubounas!\n");
+		exit(1);
+
+	}
+
+	for( i = 0 ; i < len1 - (len2-1) ; i++ )
+	{
+
+			found_it = 1;
+			for( k=0 ; k < len2 ; k++ )
+			{
+				if(seq[ i+k ] == penta[ k ])
+				{
+						found_it = 0;
+				}
+			}
+
+		if( seq[i] == penta[ 0 ] &&
+			 seq[i+1] == penta[ 1 ] &&
+			 seq[i+2] == penta[ 2 ] &&
+			 seq[i+3] == penta[ 3 ] &&
+			 seq[i+4] == penta[ 4 ])
+		{
+		printf("Found it at %d\n", i+1 );
+		}
+	}
+
+
+
+}
+
