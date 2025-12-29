25c6ab8c0062c1040d3596d0af6335ed431d7aaf
GeorgiosPapavasileiou1 <georpapa167@mbg.duth.gr>
2025-12-29 23:15:55 +0200

    Add files via upload

---
+++ p04.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#inlude <string.h>
+
+
+int main()
+{
+	char seq[ 1000 ];
+
+	scanf("%s", seq);
+	int len;
+
+	printf("The length  was : %d \n", len);
+
+	len = strlen(seq);
+
+
+}
+
---
+++ skel.c
@@ -0,0 +1,9 @@
+#include <stdioh>
+
+int main()
+{
+
+
+
+}
+
